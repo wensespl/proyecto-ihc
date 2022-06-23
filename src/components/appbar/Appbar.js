ece52e7e9d52db1b311c58e83b649f10d8db2ede
wenses-pl <wpenadillol@agiletech.com.pe>
2022-06-23 00:03:22 -0500

    update  appbar

--- src/components/appbar/Appbar.js
+++ src/components/appbar/Appbar.js
@@ -165,7 +165,10 @@ export const Appbar = () => {
               <Box sx={{ flexGrow: 0 }}>
                 <Tooltip title="Open settings">
                   <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar alt="User Name" src="/static/images/avatar/2.jpg" />
+                    <Avatar
+                      alt={nombres[0]}
+                      // src="/static/images/avatar/2.jpg"
+                    />
                   </IconButton>
                 </Tooltip>
                 <Menu
