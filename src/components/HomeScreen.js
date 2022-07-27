14986346a17b6c803dbb9adda37b8f755fcc41bc
wenses-pl <wpenadillol@agiletech.com.pe>
2022-07-27 13:18:39 -0500

    update

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -4,42 +4,38 @@ import React from 'react'
 export const HomeScreen = () => {
   return (
     <Container sx={{ marginTop: 5 }}>
-      <Typography variant="h3">
+      <Typography variant="h3" sx={{ marginBottom: 3 }}>
         comandos permitidos son las frases que contengan lo siguiente:
       </Typography>
-      <Typography variant="h5">
-        “login”: Redirige a la página de login.
-      </Typography>
-      <Typography variant="h5">
-        “register”: Redirige a la página de registro.
-      </Typography>
-      <Typography variant="h5">
+      <Typography>“login”: Redirige a la página de login.</Typography>
+      <Typography>“register”: Redirige a la página de registro.</Typography>
+      <Typography>
         “home”: Redirige a la página de inicio dependiendo de si está logueado o
         no.
       </Typography>
-      <Typography variant="h5">“log out”: Cierra la sesión actual.</Typography>
-      <Typography variant="h5">
+      <Typography>“log out”: Cierra la sesión actual.</Typography>
+      <Typography>
         “create new course”: Abre el modal con el formulario para crear un nuevo
         curso.
       </Typography>
-      <Typography variant="h5">
+      <Typography>
         “add new content”: Abre el modal con el formulario para añadir un nuevo
         contenido al curso.
       </Typography>
-      <Typography variant="h5">
+      <Typography>
         “my courses”: Redirige a la página principal (inicio) donde se muestran
         todos los cursos en los que estés añadido o hayas creado según tu rol
         correspondiente.
       </Typography>
-      <Typography variant="h5">
+      <Typography>
         “course content”: Redirige a la página del contenido del curso que estás
         viendo actualmente.
       </Typography>
-      <Typography variant="h5">
+      <Typography>
         “comments”: Redirige a la página de comentarios del curso que estás
         viendo actualmente.
       </Typography>
-      <Typography variant="h5">
+      <Typography>
         “search courses”: Redirige a la página de buscar cursos.
       </Typography>
     </Container>
